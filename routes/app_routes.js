88bcfc9dd606ea29650fbd6fc4b6b5e4ffd6a6a1
Ashraf50 <ashrafesam00@gmail.com>
2025-01-23 09:51:08 +0200

    fetch data from supabase

--- routes/app_routes.js
+++ routes/app_routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const database=require("../db/db_manager");
+const supeData=require("../supabase/fetch_data")
 const middle = require("../middleware/api_key");
 const router = express.Router();
 
@@ -8,6 +9,8 @@ res.json({"message":"Welcome to..."});
 });
 
 router.get("/services", middle.apiKeyMiddleware, database.getData);
+
+router.get("/services/supabase", supeData.fetchData);
 
 module.exports = {
     router,
