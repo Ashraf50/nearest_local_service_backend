9290c25898027d9a8213f6c2add2ca2e7a303a17
Ashraf50 <ashrafesam00@gmail.com>
2025-01-23 10:22:21 +0200

    add all data

--- routes/app_routes.js
+++ routes/app_routes.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const database=require("../db/db_manager");
-const supeData=require("../supabase/fetch_data")
+const database = require("../db/db_manager");
+const supeData = require("../supabase/fetch_data");
 const middle = require("../middleware/api_key");
 const router = express.Router();
 
-router.get("/", (req,res)=>{
-res.json({"message":"Welcome to..."});
+router.get("/", (req, res) => {
+    res.json({ message: "Welcome to..." });
 });
 
 router.get("/services", middle.apiKeyMiddleware, database.getData);
 
-router.get("/services/supabase", supeData.fetchData);
+router.get("/api/services/", middle.apiKeyMiddleware, supeData.fetchData);
 
 module.exports = {
     router,
--- supabase/fetch_data.js
+++ supabase/fetch_data.js
@@ -2,20 +2,19 @@ const { createClient } = require("@supabase/supabase-js");
 const supabase = createClient(process.env.SUBA_URL, process.env.SUBA_KEY);
 
 async function fetchData(req, res) {
-    const  type  = req.query.type;
+    const type = req.query.type;
     console.log("Type passed:", type);
     const { data, error } = await supabase
-      .from("banks")
-      .select("*")
-      .eq("type", type);
+        .from("services")
+        .select("*")
+        .eq("type", type);
     if (error) {
-      return res
-        .status(500)
-        .json({ error: "Error fetching data", details: error });
+        return res
+            .status(500)
+            .json({ error: "Error fetching data", details: error });
     }
-  
-    res.json(data);
-  }
+    res.json({length: data.length,data: data});
+}
 module.exports = {
-  fetchData,
+    fetchData,
 };
